f5a75732c49f633ec27594155355ee25a7e4a193
ch0k0bn <aquint@quarkslab.com>
2017-09-01 10:25:58 +0200

    Release version 1.1.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
         return f.read()
 
 setup(name='irmacl',
-      version='1.1.5',
+      version='1.1.6',
       description='Irma command line tool for API v1.1',
       long_description=readme(),
       url='https://github.com/quarkslab/irma-cli',

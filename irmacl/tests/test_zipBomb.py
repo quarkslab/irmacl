b466c01e56fc22efa5030f95df03e89dfef562db
ichatelier <ichatelier@quarkslab.com>
2018-04-19 11:59:06 +0200

    Add zipbomb test
    Closes IRMA-403

---
+++ irmacl/tests/test_zipBomb.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import unittest
+import os
+from irmacl.helpers import scan_files, \
+    scan_proberesults, probe_list
+
+cwd = os.path.dirname(__file__)
+SAMPLES_DIR = os.path.join(cwd, "samples")
+ZIP_SAMPLE = "zipbomb.zip"
+
+
+class TestZipBomb(unittest.TestCase):
+
+    def test_zipbomb(self):
+        probelist = probe_list()
+        probe = u'Unarchive'
+        if probe not in probelist:
+            raise unittest.SkipTest("Skipping %s not present" % probe)
+        force = True
+        sample = os.path.join(SAMPLES_DIR, ZIP_SAMPLE)
+        scan = scan_files([sample], force, probe=[probe], blocking=True)
+        self.assertEqual(len(scan.results), 1)
+        self.assertEqual(scan.probes_finished, 1)
+        result = scan_proberesults(scan.results[0].result_id)
+        self.assertEqual(len(result.probe_results), 1)
+        probe_result = result.probe_results[0]
+        self.assertEqual(probe_result.status, -1)
+        self.assertNotEqual(probe_result.error, None)
+        self.assertEqual(probe_result.results, None)
+
+
+if __name__ == "__main__":
+    unittest.main()
